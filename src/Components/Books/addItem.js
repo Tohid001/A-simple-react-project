8df3e889802dc94d26f5c619aef8a367b7f193e2
Tohidul Islam <mdtohid.sust@gmail.com>
2021-05-19 03:24:42 +0600

    converted the whole project into functional component

--- src/Components/Books/addItem.js
+++ src/Components/Books/addItem.js
@@ -1,56 +1,53 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { UserContext } from "../Context/context";
 
-export default class AddItems extends React.Component {
-  state = { selectValue: "Chose books..." };
-  handleChange = (event) => {
-    this.setState({ selectValue: event.target.value });
+export default function AddItems() {
+  const { books, handleDummy, handleReset } = useContext(UserContext);
+
+  const [localState, setLocalState] = useState("Chose books...");
+
+  const handleChange = (event) => {
+    setLocalState(event.target.value);
   };
-  handleAddItems = () => {
-    if (this.state.selectValue !== "Chose books...") {
-      this.context.handleDummy(this.state.selectValue);
-      this.setState({ selectValue: "Chose books..." });
+  const handleAddItems = () => {
+    if (localState !== "Chose books...") {
+      handleDummy(localState);
+      setLocalState("Chose books...");
     }
   };
 
-  handleReset = () => {
-    this.setState({ selectValue: "Chose books..." });
-    this.context.handleReset();
+  const handleReset2 = () => {
+    setLocalState("Chose books...");
+    handleReset();
   };
 
-  render() {
-    const { selectValue } = this.state;
-    const { books } = this.context;
-    return (
-      <div className="d-flex justify-content-center mb-3">
-        <nav className="navbar p-0 navbar-light bg-light">
-          <select
-            class="form-control d-inline-block mr-sm-2 custom-select"
-            style={{ width: "auto" }}
-            value={selectValue}
-            onChange={this.handleChange}
-          >
-            <option>Chose books...</option>
-            {books.map((book) => {
-              return <option>{book.name}</option>;
-            })}
-          </select>
-          <button
-            className="btn btn-outline-success my-2 my-sm-0"
-            onClick={this.handleAddItems}
-          >
-            Add to list
-          </button>
-          <button
-            className="btn btn-outline-primary my-2 my-sm-0"
-            onClick={this.handleReset}
-          >
-            Reset
-          </button>
-        </nav>
-      </div>
-    );
-  }
+  return (
+    <div className="d-flex justify-content-center mb-3">
+      <nav className="navbar p-0 navbar-light bg-light">
+        <select
+          class="form-control d-inline-block mr-sm-2 custom-select"
+          style={{ width: "auto" }}
+          value={localState}
+          onChange={handleChange}
+        >
+          <option>Chose books...</option>
+          {books.map((book) => {
+            return <option>{book.name}</option>;
+          })}
+        </select>
+        <button
+          className="btn btn-outline-success my-2 my-sm-0"
+          onClick={handleAddItems}
+        >
+          Add to list
+        </button>
+        <button
+          className="btn btn-outline-primary my-2 my-sm-0"
+          onClick={handleReset2}
+        >
+          Reset
+        </button>
+      </nav>
+    </div>
+  );
 }
-
-AddItems.contextType = UserContext;
--- src/Components/Context/context.js
+++ src/Components/Context/context.js
@@ -1,82 +1,92 @@
 // import React, { Component } from "react";
-import React, { createContext } from "react";
+import React, { createContext, useReducer } from "react";
 
 let Context = null;
 const { Provider, Consumer } = (Context = createContext());
 
-class UserProvider extends React.Component {
-  state = {
-    books: [
-      {
-        name: "Manufacturig Engg.",
-        author: "Mr. X",
-        price: "100$",
-        quantity: 10,
-        id: 1,
-      },
-      {
-        name: "Operations Management",
-        author: "Mr. Y",
-        price: "150$",
-        quantity: 15,
-        id: 2,
-      },
-      {
-        name: "Differential Calculus",
-        author: "Mr. Z",
-        price: "200$",
-        quantity: 20,
-        id: 3,
-      },
-    ],
-    dummyBooks: null,
-  };
+const initialState = {
+  books: [
+    {
+      name: "Manufacturig Engg.",
+      author: "Mr. X",
+      price: "100$",
+      quantity: 10,
+      id: 1,
+    },
+    {
+      name: "Operations Management",
+      author: "Mr. Y",
+      price: "150$",
+      quantity: 15,
+      id: 2,
+    },
+    {
+      name: "Differential Calculus",
+      author: "Mr. Z",
+      price: "200$",
+      quantity: 20,
+      id: 3,
+    },
+  ],
+  dummyBooks: null,
+};
+const reducer = (state, { type, value }) => {
+  switch (type) {
+    case "dummyBooks":
+      return { ...state, dummyBooks: value };
 
-  handleDummy = (name) => {
-    if (!this.state.dummyBooks) {
-      const newDummy = this.state.books.filter((book) => {
+    default:
+      return state;
+  }
+};
+
+function UserProvider(props) {
+  const [book, dispatch] = useReducer(reducer, initialState);
+
+  const handleDummy = (name) => {
+    if (!book.dummyBooks) {
+      const newDummy = book.books.filter((book) => {
         return book.name === name;
       });
-      this.setState({ dummyBooks: newDummy });
+      dispatch({ type: "dummyBooks", value: newDummy });
     } else {
       if (
-        !this.state.dummyBooks.some((book) => {
+        !book.dummyBooks.some((book) => {
           return book.name === name;
         })
       ) {
-        const newDummy = this.state.books.filter((book) => {
+        const newDummy = book.books.filter((book) => {
           return book.name === name;
         });
-        const NewDummy = [...this.state.dummyBooks];
+        const NewDummy = [...book.dummyBooks];
         NewDummy.push(newDummy[0]);
-        this.setState({ dummyBooks: NewDummy });
+        dispatch({ type: "dummyBooks", value: NewDummy });
       } else {
         alert("Item already selected!");
       }
     }
   };
-  handleState = (id) => {
-    let newBooks = this.state.dummyBooks.filter((book) => {
+  const handleState = (id) => {
+    let newBooks = book.dummyBooks.filter((book) => {
       return book.id !== id;
     });
-    this.setState({ dummyBooks: newBooks });
+    dispatch({ type: "dummyBooks", value: newBooks });
   };
-  handleReset = () => {
-    this.setState({ dummyBooks: null });
+  const handleReset = () => {
+    dispatch({ type: "dummyBooks", value: null });
   };
-  render() {
-    return (
-      <Provider
-        value={{
-          ...this.state,
-          handleState: this.handleState.bind(this),
-          handleDummy: this.handleDummy.bind(this),
-          handleReset: this.handleReset.bind(this),
-        }}
-      >
-        {this.props.children}
-      </Provider>
-    );
-  }
+  return (
+    <Provider
+      value={{
+        ...book,
+        dispatch,
+        handleState,
+        handleDummy,
+        handleReset,
+      }}
+    >
+      {props.children}
+    </Provider>
+  );
 }
 export { UserProvider, Consumer as UserConsumer, Context as UserContext };
